d32f125b99f89cb07cafc5aee8c4ff1312a91cd9
KaushAAAAAAAAAAAL <160282634+KaushaL-09V@users.noreply.github.com>
2026-03-18 23:54:38 +0530

    Modify Flask for production

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,4 @@ def predict():
     return jsonify({'prediction': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
